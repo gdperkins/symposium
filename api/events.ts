1c2c02265896c22f8b589c39ac13047679c30495
gdp <grantdperkins@gmail.com>
2025-06-28 17:12:14 +0700

    Corrected api folder

---
+++ api/events.ts
@@ -0,0 +1,9 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const { name = 'World' } = req.query;
+
+  res.status(200).json({
+    message: `Hello ${name}!`,
+  });
+}
